12f562834c44a2ead9d6c874a7aabcc01c501a5e
d.a.melkov <d.a.melkov@gmail.com>
2019-02-05 16:59:31 +0300

    M: Исправлены ошибки сборки.

--- app/src/main/java/ru/mono/MainActivity.java
+++ app/src/main/java/ru/mono/MainActivity.java
@@ -3,11 +3,17 @@ package ru.mono;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import ru.mono.controller.IGame;
+import ru.mono.model.GameFactory;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        IGame game = GameFactory.create();
+        game.start();
     }
 }
